020d551771c48f52cd98f03b8a15d1195f604feb
cosmeenelul <cosmi.gabriel212004@gmail.com>
2025-12-25 21:05:19 +0200

    modify server.js

--- server.js
+++ server.js
@@ -5,17 +5,16 @@ const app = express();
 app.use(express.json());
 app.use(express.static('public'));
 
-const url = 'mongodb://admin:password@localhost:27017';
+const url = 'mongodb://admin:password@mongo-db:27017';
 const client = new MongoClient(url);
 const dbName = 'proiect_pi';
 
-// --- RUTA POST: Primește temperatura și o salvează ---
 app.post('/api/temperatura', async (req, res) => {
     try {
         await client.connect();
         const db = client.db(dbName);
         
-        // Luăm datele trimise din "corp" (body)
+        
         const nouaInregistrare = {
             valoare: req.body.temperatura,
             unitate: "Celsius",
@@ -31,13 +30,13 @@ app.post('/api/temperatura', async (req, res) => {
     }
 });
 
-// --- RUTA GET: Ia toate temperaturile din DB ---
+
 app.get('/api/temperatura', async (req, res) => {
     try {
         await client.connect();
         const db = client.db(dbName);
         
-        // Sortăm după dată (-1 înseamnă cele mai recente primele)
+    
         const date = await db.collection('istoric_temp').find().sort({ data: -1 }).toArray();
         
         res.json(date);
@@ -49,5 +48,5 @@ const PORT = 3000
 const HOST = '0.0.0.0'
 
 app.listen(PORT, HOST, () => {
-    console.log('Serverul rulează pe http://0.0.0.0:3000');
+    console.log('Serverul ruleaza pe http://0.0.0.0:3000');
 });
